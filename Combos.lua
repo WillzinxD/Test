122eb8fd1dc975705421a33def2f7f948f78f697
WillzinxD <130069211+WillzinxD@users.noreply.github.com>
2023-04-06 11:08:38 -0300

    Add files via upload

---
+++ Combos.lua
@@ -0,0 +1,143 @@
+local warTab = addTab("Combos")
+
+-- tools tab
+setDefaultTab("Combos")
+
+macro(200, "Combo Itachi", function()
+ if g_game.isAttacking() then
+ say('Enton:Yasaka No Magatama') 
+ say('Enton:Susano Kogeki')
+ say('Katon Ryuka No Jutsu Max')
+ say('Katon:Hosenka Tsumaberi')
+ say('Amaterasu')
+end
+end)
+
+macro(100, "Mei", function()
+ if g_game.isAttacking() then
+ say('Suiton Gekiryudan')
+ say('Suiton Ryusuiben')
+ say('Suiton Eki shizuku ransu')
+ say('Suichutotsu')
+end
+end)
+
+macro(200, "Madara", function()
+ if g_game.isAttacking() then
+ say('Tenso Tsuisei')
+ say('Hijutsu Jubaku Eisou') 
+ say('Susano Attack')
+ say('Katon:Hosenka Tsumaberi')
+ say('Mokuton Jukai Heki')
+end
+end) 
+
+macro(200, "Killer Bee", function()
+ if g_game.isAttacking() then
+ say('Raiton:Hachibi Energy')
+ say('Raiton:Flash Sword') 
+ say('Raiton:Double Lariat')
+ say('Raiton:Lariat')
+ say('Raiton:Chinbou')
+end
+end) 
+
+macro(200, "Shisui", function()
+ if g_game.isAttacking() then
+ say('Susanoo Tsukumo')
+ say('Susanoo Danmaku')
+ say('Katon Karyu Endan')
+ say('Katon Ryuen Goka ')
+end
+end)
+
+macro(200, "Raikage c/Reta", function()
+ if g_game.isAttacking() then
+ say('Raiton:Ningu Sutoreto')
+ say('Raiton:Renger Punch')
+ say('Raiton:Double Lariat')
+ say('Raiton:Raiger Bomb')
+ say('Raiton:Soturo Shiden')
+end
+end)
+
+macro(100, "Minato", function()
+ if g_game.isAttacking() then
+ say('Taikyoku Rasengan')
+ say('flash rasengan') 
+ say('Hiraishin Ni No Dan')
+ say('kyuubi Punch') 
+ say('Chodan Rasengan')
+end
+end)
+
+macro(100, "Guren", function()
+ if g_game.isAttacking() then
+ say('Shoton:Suisho Kyumio Roi Rokkakuchu')
+ say('Shoton:Suishoheki Hachi No Jin') 
+ say('Shoton:Kessho Gokakuro')
+ say('Shoton:Isshi Komyo') 
+ say('Shoton:Karyuu Ryuken')
+end
+end)
+
+macro(200, "Guy C/Area", function()
+ if g_game.isAttacking() then
+ say('Senpo Yagai')
+ say('Yagai')
+ say('Sekizo')
+ say('asa kujaku')
+ say('Koyubi Kougeki')
+ say('Hirudora')
+end
+end)
+
+macro(200, "Muu", function()
+ if g_game.isAttacking() then
+ say('Doton:Jinton Genkai Hakuri ')
+ say('Doton:Kajugan ')
+ say('Jinton: Genkai Hakuri no Jutsu')
+ say('Doton: Doryuha ')
+ say('Doton: Keijugan no Jutsu')
+end
+end)
+
+macro(200, "Naruto", function()
+ if g_game.isAttacking() then
+ say('Senpou Youton Rasenshuriken ')
+ say('Kyuubi Rasengan ')
+ say('Fuuton Rasenshuriken ')
+ say('Biju Rasengan ')
+ say('Rasenshuriken ')
+end
+end)
+
+macro(200, "Sakura", function()
+ if g_game.isAttacking() then
+ say('Chuusuusei kyaku Taihou ')
+ say('Taihou Mass Punch ')
+ say('Shanaro Punch ')
+ say('Ground Fubuki Katsugami ')
+ say('Fubuki Katsugami ')
+end
+end)
+
+macro(200, "Shisui", function()
+ if g_game.isAttacking() then
+ say('Susanoo Tsukumo')
+ say('Susanoo Danmaku')
+ say('Katon Karyu Endan')
+ say('Katon Ryuen Goka ')
+end
+end)
+
+macro(200, "Kaguya", function()
+ if g_game.isAttacking() then
+ say('Tomogoroshi No Haikotsu')
+ say('Yomotsu Kami No Ai')
+ say('Shikotsumyaku')
+ say('Tomogoroshi Tsoushou')
+ say('Yomotsu Hirasaka')
+end
+end)
+
